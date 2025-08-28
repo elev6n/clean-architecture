8187714cc6ce62a77817cc083e759ad827aad769
Владимир Сорокин <vovasorokin148@gmail.com>
2025-08-28 17:04:17 +0300

    add: User and DomainException

--- CleanArchitecture.Domain/Entities/User.cs
+++ CleanArchitecture.Domain/Entities/User.cs
@@ -1,14 +1,35 @@
+using CleanArchitecture.Domain.Exceptions;
+
 namespace CleanArchitecture.Domain.Entities;
 
 public class User
 {
     public int Id { get; private set; }
 
-    public string Email { get; private set; }
+    public string Email { get; private set; } = null!;
 
-    public string FirstName { get; private set; }
+    public string FirstName { get; private set; } = null!;
 
-    public string LastName { get; private set; }
+    public string LastName { get; private set; } = null!;
 
     public DateTime CreatedAt { get; private set; }
+
+    private User() { }
+
+    public User(string email, string firstName, string lastName)
+    {
+        if (string.IsNullOrEmpty(email))
+            throw new DomainException("Email is required");
+
+        Email = email;
+        FirstName = firstName;
+        LastName = lastName;
+        CreatedAt = DateTime.Now;
+    }
+
+    public void UpdateName(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
 }
